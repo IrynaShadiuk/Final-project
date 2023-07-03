61636eec7fb2646d42cc7ea9ad13ffef7ba89ce8
IrynaShadiuk <shaduk3@gmail.com>
2023-07-03 22:15:49 +0300

    add React to project; add typeScript and SASS

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+       {/* <Header />
+       <Main />
+       <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
